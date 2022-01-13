0c84e9371d3a29bcf5e2bf0ced13a0a061dcf15c
Roy Rinberg <royrinberg@gmail.com>
2022-01-13 01:59:24 -0500

    copy over arbitrary code into files to the EC2 instance, and execute it

--- cloud_compute_utils/run_code.py
+++ cloud_compute_utils/run_code.py
@@ -1,25 +1,16 @@
 #from cloud_compute_utils import aws_util
-import paramiko
+
 import sys
 import os
+from pathlib import Path
 import socket
 from cloud_compute_utils import run_remote_code
 from cloud_compute_utils import aws_util
-"""
-
-1. run function on instance
-2. save code to s3 bucket
-
-to do: for the instances, have them be a specific size; have them mount to a specific bucket
-    1. pass arguments (in this case, the indicies of the data), 
-    2. pull code from github,  run poetry install
-    which takes indices as an argument, 
-
-
-1. write a decorator function that writes the function to s3
-    1. function needs to return dict
-    2. function needs to return numpy array
-
+"""Todo:
+    2. save code to s3 bucket
+    1. write a decorator function that writes the function to s3
+        1. function needs to return dict
+        2. function needs to return numpy array
 """
 
 key_filepath = "/home/roy/code/research/cloud_compute_utils/secrets/ec2-keypair.pem"
@@ -44,70 +35,63 @@ def run_this_file_on_instance(hostname, username, key_filepath):
                                             key_filepath=key_filepath)
 
 
-def helper():
-    print(os.name)
-    print(socket.gethostname())
+def get_run_cmd_str():
+    return '''
+#!/bin/bash
+set -x
+
+cd $HOME
+cd cloud_compute_utils
+echo `/home/ubuntu/.local/bin/deployments-cli` > deployments-cli.txt
+    '''
+
+
+def get_install_cmd_str():
+    return '''
+#!/bin/bash
+set -x
+sudo apt update
+sudo apt install -y python3-pip
+
+cd $HOME
+git clone https://github.com/RoyRin/cloud_compute_utils
+pip install cloud_compute_utils
+    '''
+
+
+def copy_cmd_to_file(cmd, filename):
+    return f"echo \'{cmd}\' > {filename}"
 
 
 if __name__ == '__main__':
-    """
-    try:
-
-        if sys.argv[1] == 'deploy':
-            command_str = "python3 /tmp/myscript.py"
-            run_this_file_on_instance(hostname=hostname,
-                                      username=username,
-                                      key_filepath=key_filepath)
-            sys.exit(0)
-    except IndexError:
-        pass
-    """
-
     ec2 = aws_util.get_ec2_client()
     running_instances = aws_util.get_running_instances(ec2)
     hostname = running_instances[0].public_dns_name
-    #hostname = "ec2-44-201-236-146.compute-1.amazonaws.com"
     username = "ubuntu"
 
     aws_util.print_instance(running_instances[0])
 
-    install_cmds = [
-        #"sudo apt update", "sudo apt install -y python3-pip",
-        #"curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -",
-        "pwd",
-        f"git clone {github}",
-        f"cd {github_dir}",
-        "ls",
-        f"poetry shell",
-        "poetry install",
-        f"deployment-cli --help"
-    ]
+    basedir = Path(__file__).parent.resolve()
+    scripts_dir = basedir / "scripts"
+    print(scripts_dir)
+
     local_to_remote_filenames = {
-        "/home/roy/code/research/cloud_compute_utils/cloud_compute_utils/scripts/install_cmd.sh":
-        "/tmp/install.sh",
-        "/home/roy/code/research/cloud_compute_utils/cloud_compute_utils/scripts/run.sh":
-        "/tmp/run.sh"
+        os.path.join(scripts_dir, "install_cmd.sh"):
+        os.path.join("/tmp", "install_cmd.sh"),
+        os.path.join(scripts_dir, "run.sh"):
+        os.path.join("/tmp", "run.sh"),
     }
 
-    command_str_install = "bash /tmp/install_cmd.sh"
-    command_str_run = "bash /tmp/run.sh"
-    run_remote_code.copy_files_to_instance(
-        local_to_remote_filenames=local_to_remote_filenames,
-        hostname=hostname,
-        username=username,
-        key_filepath=key_filepath)
+    install_str = "bash /tmp/install_cmd.sh"
+    run_str = "bash /tmp/run_cmd.sh"
 
+    s = f"my name is Roy {basedir}"
+    cmd = f"echo \"{s}\" > /tmp/my_file.txt"
+    cmd1 = copy_cmd_to_file(get_install_cmd_str(), "/tmp/install_cmd.sh")
+    cmd2 = copy_cmd_to_file(get_run_cmd_str(), "/tmp/run_cmd.sh")
     run_remote_code.run_command_on_instance(
-        command_strings=[command_str_install, command_str_run],
+        command_strings=[cmd1, cmd2, install_str, run_str],
         hostname=hostname,
         username=username,
         key_filepath=key_filepath,
         verbose=True)
-    """command_str
-    run_remote_code.run_command_on_instance(command_strings=install_cmds,
-                                            hostname=hostname,
-                                            username=username,
-                                            key_filepath=key_filepath,
-                                            verbose=True)
-
-    """
--- cloud_compute_utils/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-cd $HOME
-
-cd cloud_compute_utils
-#poetry shell
-echo `/home/ubuntu/.local/bin/deployments-cli` > deployments-cli.txt
